0fb0679bc72774d99d145cfb0cb1f9775f7bb71a
ffang <ffang@illumina.com>
2019-08-21 17:51:28 +0800

    success to generate html table

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 # file_input='/c/BeyondsoftProjects/hera/integration-api-test/target/IngestionResults/2019-08-15.json'
 # file_output='/c/BeyondsoftProjects/hera/integration-api-test/target/IngestionResults/table.html'
-file_input='/c/shell/2019-08-15.json'
+file_input='/c/shell/2019-08-15.json'	# {"results":[{"caseId":"id-1","status":"READY","Message":null,"ingestionTime":123},{"caseId":"id-2","status":"READY","Message":null,"ingestionTime":456}]}
 file_output='/c/shell/table.html'
 
 file_temp1='/c/shell/temp1.txt'
@@ -12,81 +12,83 @@ file_temp3='/c/shell/temp3.txt'
 resultsArray=''
 headNames=''
 
-# for file in /c/BeyondsoftProjects/hera/integration-api-test/target/IngestionResults/2019-08-15.json
-for file in $file_input
-do
-	#echo $file
-	#cat $file
-	# echo $(basename ${file})
-	
-	#echo $(awk -F ':\\[\\{' '{print $2}' $file) | awk -F '\\}\\]\\}' '{print $1}'
-	
-	#awk -F ':\\[\\{' '{print $2}' $file | awk -F '\\}\\]\\}' '{print $1}' | awk -F '\\},\\{' '{print $1, $2}'
-	
-	# awk -F ':\\[\\{' '{print $2}' $file | awk -F '\\}\\]\\}' '{print $1}' | awk -F '\\},\\{' '{i=1; while(i<=NF) {print "<td>"$i"</td>";i++}}'
-	
-	resultsArray=($(awk -F ':\\[\\{' '{print $2}' $file | awk -F '\\}\\]\\}' '{print $1}'))
-	echo "results: "$resultsArray
-	
-	# resultsArray=`echo "aaa},{bbb"`
-	
-	# echo $resultsArray | awk -F '\\},\\{' '{i=1; while(i<=NF) {print "<td>"$i"</td>";i++}}'
-	
-done
+
+function initial(){
+	for file in $file_input
+	do
+		#echo $file
+		#cat $file
+		# echo $(basename ${file})
+		
+		#echo $(awk -F ':\\[\\{' '{print $2}' $file) | awk -F '\\}\\]\\}' '{print $1}'
+		#awk -F ':\\[\\{' '{print $2}' $file | awk -F '\\}\\]\\}' '{print $1}' | awk -F '\\},\\{' '{i=1; while(i<=NF) {print "<td>"$i"</td>";i++}}'
+
+		resultsArray=($(awk -F ':\\[\\{' '{print $2}' $file | awk -F '\\}\\]\\}' '{print $1}'))
+		# echo "results: "$resultsArray
+		# resultsArray is: "caseId":"id-1","status":"READY","Message":null,"ingestionTime":123},{"caseId":"id-2","status":"READY","Message":null,"ingestionTime":456
+	done
+}
 
 function create_html_head(){
-	echo -e "<html>
-				<body>
-					<h1><center>Ingestion Results</center></h1>
-					<center>
-						<table border='1'>"
+	echo "<html>
+	<body>
+		<h1><center>Ingestion Results</center></h1>
+			<center>
+				<table border='1'>"
 }
 
 function create_table_head(){
-	echo "<tr>"
+	# both of the two ways can get: <tr><th>caseId</th><th>status</th><th>Message</th><th>ingestionTime</th></tr>
+	# echo "<tr>"
+	# heads=`echo $resultsArray | awk -F '},{' '{print $1}' | awk 'BEGIN{FS=","} {i=1; while(i<=NF) {split($i, keyy, ":"); split(keyy[1], key, "\""); print "<th>"key[2]"</th>";i++}}'`
+	# echo $heads
+	# echo "</tr>"
 
-	# resultsArray is: "caseId":"id-1","status":"READY","Message":null,"ingestionTime":123},{"caseId":"id-2","status":"READY","Message":null,"ingestionTime":456
-
-	heads=`echo $resultsArray | awk -F '},{' '{print $1}' | awk 'BEGIN{FS=","} {i=1; while(i<=NF) {split($i, keyy, ":"); split(keyy[1], key, "\""); print "<td>"key[2]"</td>";i++}}'`
-	# heads=`echo $resultsArray | awk -F '},{' '{print $1}' | awk -F ',' '{i=1; while(i<=NF) {split($i, keyy, ":"); split(keyy[1], key, "\""); print "<td>"key[2]"</td>";i++}}'`
-	echo $heads		# will get: <td>caseId</td> <td>status</td> <td>Message</td> <td>ingestionTime</td>
-	
-	echo "</tr>"
+	echo $resultsArray | awk -F '},{' '{print $1}' | awk -F ',' '{
+		print "<tr>";
+		i=1; 
+		while(i<=NF) {
+			split($i, keyy, ":"); 
+			split(keyy[1], key, "\""); 
+			print "	<th>"key[2]"</th>";
+			i++;
+		}
+		print "</tr>";
+	}'
 }
 
 function create_table_rows(){
-	echo "<tr>"
-
-	# resultsArray is: "caseId":"id-1","status":"READY","Message":null,"ingestionTime":123},{"caseId":"id-2","status":"READY","Message":null,"ingestionTime":456
-
-	rows=`echo $resultsArray | awk -F '},{' '{print $2}' | awk '
-		BEGIN{FS=","} 
-		{
-			i=1; 
-			while(i<=NF) {
-				split($i, valuee, ":"); 
-				if(index(valuee[2],"\"")==1){
-					split(valuee[2], value, "\""); 
-					print "<td>"value[2]"</td>";
+	# will get: <tr><td>id-1</td><td>READY</td><td>null</td><td>123</td></tr><tr><td>id-2</td><td>READY</td><td>null</td><td>456</td></tr>
+	echo $resultsArray | awk -F '},{' '{
+		i=1;
+		while(i<=NF) {
+			print "<tr>";
+			count=split($i, keyValues, ",");
+			j=1;
+			while(j<=count) {
+				split(keyValues[j], keyValue, ":"); 
+				if(index(keyValue[2],"\"")==1){
+					split(keyValue[2], value, "\""); 
+					print "	<td>"value[2]"</td>";
 				}
 				else{
-					print "<td>"valuee[2]"</td>";
+					print "	<td>"keyValue[2]"</td>";
 				}
-				i++;
+
+				j++;
 			}
+
+			print "</tr>";
+			i++;
 		}
-		'
-		`
-	echo $rows		# will get: <td>id-1</td> <td>READY</td> <td>null</td> <td>123</td>
-	
-	echo "</tr>"
+	}'
 }
 
 function create_html_end(){
-	echo "</table>
-		</center>
-	</body>
-</html>"
+	echo "				</table>
+			</center>
+		</body>
+	</html>"
 }
 
 function create_html(){
@@ -97,10 +99,7 @@ function create_html(){
 		
 	create_table_rows >> $file_output
 	create_html_end >> $file_output
-	
 }
 
-#create_html
-
-create_table_head
-create_table_rows
+initial
+create_html
